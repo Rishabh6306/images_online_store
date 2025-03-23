db59d6c98febefd6135bca2fdf3e36477b28c6da
Rishabh Srivastava <rishabhsrivastav722@gmail.com>
2025-03-24 02:25:53 +0530

    All Schema is created

--- models/User.ts
+++ models/User.ts
@@ -1,28 +1,31 @@
 import mongoose, { Schema, model, models } from "mongoose";
 import bcrypt from "bcryptjs";
 
-export interface myUserInterface {
-    email: string,
-    password: string,
-    role: "user" | "admin",
-    _id?: mongoose.Types.ObjectId,
-    createdAt?: Date,
-    updatedAt?: Date
+export interface IUser {
+  email: string;
+  password: string;
+  role: "user" | "admin";
+  _id?: mongoose.Types.ObjectId;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
-const userSchema = new Schema<myUserInterface>({
+const userSchema = new Schema<IUser>(
+  {
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ["user", "admin"], default: "user" }
-}, { timestamps: true })
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+  },
+  { timestamps: true }
+);
 
 userSchema.pre("save", async function (next) {
-    if (this.isModified("password")) {
-        this.password = await bcrypt.hash("password", 10);
-    }
-    next();
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
 });
 
-const User = models?.User || model<myUserInterface>("User", userSchema);
+const User = models?.User || model<IUser>("User", userSchema);
 
 export default User;
