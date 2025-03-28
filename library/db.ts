fd75d9f1704145ad0feddd9f4accd4d0515c3bd4
Rishabh Srivastava <rishabhsrivastav722@gmail.com>
2025-03-28 17:59:34 +0530

    Frontend part completed

--- library/db.ts
+++ library/db.ts
@@ -1,35 +1,43 @@
 import mongoose from "mongoose";
 
-const DATABASE_URI = process.env.MONGODB_URI!;
-
+const DATABASE_URI = process.env.DATABASE_URI!;
 
 if (!DATABASE_URI) {
-    throw new Error('Check your database connection Something went wrong.');
+  throw new Error(
+    "Please define the DATABASE_URI environment variable inside .env.local"
+  );
 }
 
-let cached = global.mongooseFromGlobal;
+let cached = global.mongoose;
 
 if (!cached) {
-    cached = global.mongooseFromGlobal = { conn: null, promise: null }
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 export async function connectToDatabase() {
-    if (cached.conn) return cached.promise;
-
-    if (!cached.promise) {
-        const options = {
-            bufferCommands: true,
-            maxPoolSize: 10,
-        };
-
-        cached.promise = mongoose.connect(DATABASE_URI, options).then(() => mongoose.connection);
-    }
-
-    try {
-        cached.conn = await cached.promise;
-    } catch (error) {
-        cached.promise = null;
-    }
-
+  if (cached.conn) {
     return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: true,
+      maxPoolSize: 10,
+    };
+
+    cached.promise = mongoose
+      .connect(DATABASE_URI, opts)
+      .then(() => mongoose.connection);
+  }
+
+  try {
+      console.log("Connected to database", cached.conn);
+    cached.conn = await cached.promise;
+    
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
 }
